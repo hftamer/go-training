f3a4a6cbdafa75b7607937e07d33bcceb244311e
jesserahman <jesserahman@yahoo.com>
2021-04-14 23:59:20 -0400

    added create, update, delete functions

--- cmd/pmgr/main.go
+++ cmd/pmgr/main.go
@@ -2,23 +2,44 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"os"
 )
 
+type userMap map[string]string
+
 func main() {
 	//fmt.Println(pmgr.GetQuote())
-	fmt.Println("Argumenets", os.Args)
+	//fmt.Println("Argumenets", os.Args)
 
-	handleUserInput()
+	users := userMap{}
+	fmt.Println("user map: ", users)
+
+	users.handleUserInput()
+
+	fmt.Println("user map: ", users)
+
+	//myPwd := "shubham"
+	//providedHash, _ := HashPassword(myPwd)
+	//fmt.Println("Password :", myPwd)
+	//fmt.Println("Hash :", providedHash)
+	//
+	//isMatch := CheckPasswordHash(myPwd, providedHash)
+	//fmt.Println("Matched ?:", isMatch)
 
 }
 
-func handleUserInput(){
-	switch os := os.Args[1]; os {
+func (u userMap)handleUserInput(){
+	switch os.Args[1] {
 	case "add":
 		fmt.Println("Adding")
+		u.addUserEntry(os.Args[2], os.Args[3])
+		fmt.Println("user map: ", u)
+		u.updatePassword(os.Args[2], "4567")
+		fmt.Println("user map: ", u)
+		u.deleteUserEntry(os.Args[2])
 	case "update":
-		fmt.Println("updating.")
+		u.updatePassword("jesse@hellofresh.com", "4567")
 	case "get":
 		fmt.Println("getting")
 	case "delete":
@@ -28,6 +49,40 @@ func handleUserInput(){
 	}
 }
 
-func addPassword(){
+func (u userMap) addUserEntry(username string, password string){
+	fmt.Println("Username: ", username)
+	fmt.Println("pwd: ", password)
+	hashedPassword, error := HashPassword(password)
+	if error != nil {
+		fmt.Println("An Error Occurred")
+		os.Exit(1)
+	}
+	u[username] = hashedPassword
+}
 
+func (u userMap) updatePassword(username string, newPassword string){
+	fmt.Println("Same Username: ", username)
+	fmt.Println("updated pwd: ", newPassword)
+	newHashedPassword, error := HashPassword(newPassword)
+	if error != nil {
+		fmt.Println("An Error Occurred")
+		os.Exit(1)
+	}
+	u[username] = newHashedPassword
+}
+
+func (u userMap) deleteUserEntry(username string){
+	fmt.Println("Deleting Username: ", username)
+	delete(u, username)
+}
+
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/hftamer/go-training
 
 go 1.15
 
-require rsc.io/quote v1.5.2
+require (
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	rsc.io/quote v1.5.2
+)
