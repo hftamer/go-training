9da996b03f20a85fcd950848fb7412cee27996ab
jesserahman <jesserahman@yahoo.com>
2021-04-15 19:35:37 -0400

    add and delete functions working

--- cmd/pmgr/main.go
+++ cmd/pmgr/main.go
@@ -12,33 +12,32 @@ type userMap map[string]string
 
 func main() {
 	users := userMap{}
-	fmt.Println("user map: ", users)
 	getTextFromFile("userData.txt", users)
-	fmt.Println("user map: ", users)
+	fmt.Println("user map from file: ", users)
 
 	handleUserInput(users)
-
 }
 
 func handleUserInput(u userMap) {
 	switch os.Args[1] {
 	case "add":
-		u.addUserEntry(os.Args[2], os.Args[3])
+		addUserEntry(os.Args[2], os.Args[3])
 		fmt.Println("user map: ", u)
 	case "update":
-		u.updatePassword("user1", "4567")
+		u.updatePassword(os.Args[2], os.Args[3])
 	case "get":
 		fmt.Println("getting")
-		getPasswordFromMap(os.Args[2], u)
+		u.getPasswordFromMap(os.Args[2])
 	case "delete":
 		fmt.Println("deleting.")
-		u.deleteUserEntry(os.Args[2])
+		u.deleteUserEntry(os.Args[2], "userData.txt")
 	default:
 		fmt.Printf("You entered an invalid option")
 	}
 }
 
-func (u userMap) addUserEntry(username string, password string) {
+func addUserEntry(username string, password string) {
+	u := userMap{}
 	fmt.Println("*******  Adding ")
 	fmt.Println("Username: ", username)
 	fmt.Println("pwd: ", password)
@@ -50,13 +49,7 @@ func (u userMap) addUserEntry(username string, password string) {
 	}
 	u[username] = password
 
-	for key, value := range u {
-		fmt.Println("key", key, "value", value)
-	}
-
-	keyValuePairsAsByteSlice := createKeyValuePairsAsString(u)
-	fmt.Println("Returned value: ", keyValuePairsAsByteSlice)
-	saveTextToFile("userData.txt", keyValuePairsAsByteSlice)
+	saveUserData("userData.txt", u)
 }
 
 func createKeyValuePairsAsString(m map[string]string) string {
@@ -70,14 +63,17 @@ func createKeyValuePairsAsString(m map[string]string) string {
 	return b.String()
 }
 
-func saveTextToFile(filename string, data string) {
+func saveUserData(filename string, userInfo userMap) {
+	keyValuePairsAsByteSlice := createKeyValuePairsAsString(userInfo)
+	fmt.Println("Returned value: ", keyValuePairsAsByteSlice)
+
 	file, err := os.OpenFile(filename,
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer file.Close()
-	keyValuePairs := fmt.Sprintf("%s \n", data)
+	keyValuePairs := fmt.Sprintf("\n%s", keyValuePairsAsByteSlice)
 	if _, err := file.WriteString(keyValuePairs); err != nil {
 		fmt.Println(err)
 	}
@@ -87,14 +83,19 @@ func getTextFromFile(filename string, u userMap) {
 	byteSlice, _ := os.ReadFile(filename)
 
 	newSlice := strings.Split((string(byteSlice)),"\n")
+	fmt.Println("new slice: ", newSlice)
+	fmt.Println("length: ", len(newSlice))
 
 	for _, value := range newSlice{
-		result := strings.Split(value,"=")
-		u[result[0]]= result[1]
+		if (value != ""){
+			result := strings.Split(value,"=")
+			fmt.Println("Result: ", result)
+			u[result[0]]= result[1]
+		}
 	}
 }
 
-func getPasswordFromMap(key string, u userMap) {
+func (u userMap) getPasswordFromMap(key string) {
 	fmt.Println(u[key])
 }
 
@@ -104,16 +105,28 @@ func (u userMap) updatePassword(username string, newPassword string) {
 	fmt.Println("Same Username: ", username)
 	fmt.Println("updated pwd: ", newPassword)
 	newHashedPassword, error := HashPassword(newPassword)
+	fmt.Println("New hashed password", newHashedPassword)
+
 	if error != nil {
 		fmt.Println("An Error Occurred")
 		os.Exit(1)
 	}
-	u[username] = newHashedPassword
+	u[username] = newPassword
 }
 
-func (u userMap) deleteUserEntry(username string) {
+func (u userMap) deleteUserEntry(username string, fileName string) {
+	fmt.Println("user map coming in: ", u)
+	file, e := os.OpenFile(fileName, os.O_RDWR, 0755)
+	fmt.Println("error? ", e)
+
 	fmt.Println("Deleting Username: ", username)
 	delete(u, username)
+	fmt.Println("user map going out: ", u)
+
+	err := file.Truncate(0)
+	fmt.Println("error: ", err)
+	saveUserData("userData.txt", u)
+
 }
 
 func HashPassword(password string) (string, error) {
