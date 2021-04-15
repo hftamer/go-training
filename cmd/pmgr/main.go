0934a058c59acf12e0b4f2d9ad8f9bd054e399f3
jesserahman <jesserahman@yahoo.com>
2021-04-14 22:43:37 -0400

    added switch statement for handling command line arguments

--- cmd/pmgr/main.go
+++ cmd/pmgr/main.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/hftamer/go-training/pkg/pmgr"
+	"os"
 )
 
 func main() {
-	fmt.Println(pmgr.GetQuote())
+	//fmt.Println(pmgr.GetQuote())
+	fmt.Println("Argumenets", os.Args)
+
+	handleUserInput()
+
+}
+
+func handleUserInput(){
+	switch os := os.Args[1]; os {
+	case "add":
+		fmt.Println("Adding")
+	case "update":
+		fmt.Println("updating.")
+	case "get":
+		fmt.Println("getting")
+	case "delete":
+		fmt.Println("deleting.")
+	default:
+		fmt.Printf("You entered an invalid option")
+	}
+}
+
+func addPassword(){
+
 }
