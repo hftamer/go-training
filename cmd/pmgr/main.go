edd99a00e13c30fb9e2c8cde57062b554669ae18
jesserahman <jesserahman@yahoo.com>
2021-04-15 21:20:09 -0400

    cleaned up functions, added better error handling

--- cmd/pmgr/main.go
+++ cmd/pmgr/main.go
@@ -11,61 +11,62 @@ import (
 type userMap map[string]string
 
 func main() {
-	users := userMap{}
-	getTextFromFile("userData.txt", users)
-	fmt.Println("user map from file: ", users)
-
-	handleUserInput(users)
+	userData := userMap{}
+	filename := "userData.txt"
+	populateUserMapWithDataFromFile(filename, userData)
+	runCommandLineProgram(userData, filename)
 }
 
-func handleUserInput(u userMap) {
+func runCommandLineProgram(userData userMap, fileName string) {
 	switch os.Args[1] {
 	case "add":
-		addUserEntry(os.Args[2], os.Args[3])
-		fmt.Println("user map: ", u)
+		validateCommandLineArguments(4)
+		userData.addUserEntryToFile(os.Args[2], os.Args[3], fileName)
 	case "update":
-		u.updatePassword(os.Args[2], os.Args[3], "userData.txt")
+		validateCommandLineArguments(4)
+		userData.updatePassword(os.Args[2], os.Args[3], fileName)
 	case "get":
-		fmt.Println("getting")
-		u.getPasswordFromMap(os.Args[2])
+		validateCommandLineArguments(3)
+		userData.getPasswordFromMap(os.Args[2])
 	case "delete":
 		fmt.Println("deleting.")
-		u.deleteUserEntry(os.Args[2], "userData.txt")
+		userData.deleteUserEntry(os.Args[2], fileName)
 	default:
 		fmt.Printf("You entered an invalid option")
 	}
 }
 
-func addUserEntry(username string, password string) {
-	u := userMap{}
-	fmt.Println("*******  Adding ")
-	fmt.Println("Username: ", username)
-	fmt.Println("pwd: ", password)
-	hashedPassword, error := HashPassword(password)
-	fmt.Println("hashed pwd: (leaving out for now) ", hashedPassword)
-	if error != nil {
-		fmt.Println("An Error Occurred")
+func validateCommandLineArguments(expectedLength int) {
+	if len(os.Args) != expectedLength {
+		errorMessage := fmt.Sprintf("Wrong number of arguments: expected %v, got %v", expectedLength, len(os.Args))
+		fmt.Println(errorMessage)
 		os.Exit(1)
 	}
-	u[username] = password
-
-	saveUserData("userData.txt", u)
 }
 
-func createKeyValuePairsAsString(m map[string]string) string {
-	//A Buffer is a variable-sized buffer of bytes with Read and Write methods. The zero value for Buffer is an empty buffer ready to use.
-	b := new(bytes.Buffer)
-	for key, value := range m {
-		fmt.Fprintf(b, "%s=%s\n", key, value)
+func (userData userMap) addUserEntryToFile(username string, password string, filename string) {
+	if userData[username] != "" {
+		fmt.Println("Oops! Looks like that username already exists")
+		os.Exit(1)
 	}
-	fmt.Println("Value of B: ", b)
-	fmt.Println("B as Bytes: ", b.Bytes())
-	return b.String()
+
+	newUserData := userMap{}
+	hashedPassword,_ := HashPassword(password)
+	fmt.Println("hashed pwd: (leaving out for now) ", hashedPassword)
+	newUserData[username] = password
+	saveUserData(filename, newUserData)
+}
+
+func covertUserDataMapToString(userData userMap) string {
+	userDataAsByteSlice := new(bytes.Buffer)
+	for username, password := range userData {
+		fmt.Fprintf(userDataAsByteSlice, "%s=%s\n", username, password)
+	}
+	return userDataAsByteSlice.String()
 }
 
 func saveUserData(filename string, userInfo userMap) {
-	keyValuePairsAsByteSlice := createKeyValuePairsAsString(userInfo)
-	fmt.Println("Returned value: ", keyValuePairsAsByteSlice)
+	userDataAsOneString := covertUserDataMapToString(userInfo)
 
 	file, err := os.OpenFile(filename,
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -73,40 +74,42 @@ func saveUserData(filename string, userInfo userMap) {
 		fmt.Println(err)
 	}
 	defer file.Close()
-	keyValuePairs := fmt.Sprintf("\n%s", keyValuePairsAsByteSlice)
-	if _, err := file.WriteString(keyValuePairs); err != nil {
+
+	newUserData := fmt.Sprintf("\n%s", userDataAsOneString)
+	if _, err := file.WriteString(newUserData); err != nil {
 		fmt.Println(err)
 	}
 }
 
-func getTextFromFile(filename string, u userMap) {
-	byteSlice, _ := os.ReadFile(filename)
+func populateUserMapWithDataFromFile(filename string, u userMap) {
+	userDataAsByteSlice, _ := os.ReadFile(filename)
+	userDataAsStringSlice := strings.Split((string(userDataAsByteSlice)), "\n")
 
-	newSlice := strings.Split((string(byteSlice)), "\n")
-	fmt.Println("new slice: ", newSlice)
-	fmt.Println("length: ", len(newSlice))
-
-	for _, value := range newSlice {
+	for _, value := range userDataAsStringSlice {
 		if value != "" {
 			result := strings.Split(value, "=")
-			fmt.Println("Result: ", result)
 			u[result[0]] = result[1]
 		}
 	}
 }
 
-func (u userMap) getPasswordFromMap(key string) {
-	fmt.Println(u[key])
+func (userData userMap) getPasswordFromMap(key string) {
+	value := userData[key]
+	if value == "" {
+		fmt.Println("Oops! Looks like that username doesn't exist")
+		os.Exit(1)
+	}
+	fmt.Println(userData[key])
 }
 
-func (u userMap) updatePassword(username string, newPassword string, fileName string) {
+func (userData userMap) updatePassword(username string, newPassword string, fileName string) {
 	fmt.Println("*******  Updating ")
 	fmt.Println("Same Username: ", username)
 	fmt.Println("updated pwd: ", newPassword)
 	newHashedPassword, error := HashPassword(newPassword)
 	fmt.Println("New hashed password", newHashedPassword)
 
-	fmt.Println("user map coming in: ", u)
+	fmt.Println("user map coming in: ", userData)
 
 	file, e := os.OpenFile(fileName, os.O_RDWR, 0755)
 	fmt.Println("error? ", e)
@@ -116,28 +119,27 @@ func (u userMap) updatePassword(username string, newPassword string, fileName st
 		os.Exit(1)
 	}
 
+	userData[username] = newPassword
 
-	u[username] = newPassword
-
-	fmt.Println("user map going out: ", u)
+	fmt.Println("user map going out: ", userData)
 
 	err := file.Truncate(0)
 	fmt.Println("error: ", err)
-	saveUserData("userData.txt", u)
+	saveUserData("userData.txt", userData)
 }
 
-func (u userMap) deleteUserEntry(username string, fileName string) {
-	fmt.Println("user map coming in: ", u)
+func (userData userMap) deleteUserEntry(username string, fileName string) {
+	fmt.Println("user map coming in: ", userData)
 	file, e := os.OpenFile(fileName, os.O_RDWR, 0755)
 	fmt.Println("error? ", e)
 
 	fmt.Println("Deleting Username: ", username)
-	delete(u, username)
-	fmt.Println("user map going out: ", u)
+	delete(userData, username)
+	fmt.Println("user map going out: ", userData)
 
 	err := file.Truncate(0)
 	fmt.Println("error: ", err)
-	saveUserData("userData.txt", u)
+	saveUserData("userData.txt", userData)
 
 }
 
