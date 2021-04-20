c18041c0b05a3f533572c1bd251b1d12be3ca3fa
Tamer Aly <tamer.aly@hellofresh.com>
2021-04-20 09:39:17 -0400

    add AES encryption to the vault

--- pkg/pmgr/vault.go
+++ pkg/pmgr/vault.go
@@ -1,10 +1,20 @@
 package pmgr
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
+	"io"
 	"io/ioutil"
 )
+
+// NOTE: this is a __horrible__ security practice
+// Please do not emulate this in production!
+var cryptoPassword = "mySuperL33tpassword"
 
 type Vault struct {
 	AccountsByName map[string]string `json:"vault"`
@@ -56,31 +66,109 @@ func (vault *Vault) AddAccount(name string, pwd string) error {
 		return errors.New(name + " account already exists")
 	}
 
-	// TODO: encrypt the password first
-	vault.AccountsByName[name] = pwd
-
-	return nil
+	return vault.addPassword(name, pwd)
 }
 
 func (vault Vault) GetAccountPassword(name string) (string, error) {
-	if acc, found := vault.AccountsByName[name]; found {
-		return acc, nil
+	encryptedPwd, found := vault.AccountsByName[name]
+	if !found {
+		return "", errors.New("account " + name + " doesn't exist")
 	}
 
-	return "", errors.New("account " + name + " doesn't exist")
+	decoded, err := hex.DecodeString(encryptedPwd)
+	if err != nil {
+		return "", err
+	}
+
+	return decrypt(decoded, cryptoPassword)
 }
 
-func (vault *Vault) UpdateAccount(name string, password string) error {
-	if _, found := vault.AccountsByName[name]; found {
-		vault.AccountsByName[name] = password
-		return nil
-	} else {
+func (vault *Vault) UpdateAccount(name string, pwd string) error {
+	if !vault.IsExistingAccount(name) {
 		return errors.New("account " + name + " not found")
 	}
+
+	return vault.addPassword(name, pwd)
 }
 
 func (vault Vault) DeleteAccount(name string) error {
 	delete(vault.AccountsByName, name)
 
 	return nil
+}
+
+func (vault *Vault) addPassword(name string, pwd string) error {
+	encryptedPwd, err := encrypt([]byte(pwd), cryptoPassword)
+	if err != nil {
+		return err
+	}
+
+	vault.AccountsByName[name] = encryptedPwd
+
+	return nil
+}
+
+func encrypt(data []byte, passphrase string) (string, error) {
+	hash, err := hash(passphrase)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(hash)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+
+	return hex.EncodeToString(ciphertext), nil
+}
+
+func decrypt(data []byte, passphrase string) (string, error) {
+	hash, err := hash(passphrase)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(hash)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(data) < nonceSize {
+		return "", errors.New("nonce size exceeds data length")
+	}
+
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
+}
+
+func hash(key string) ([]byte, error) {
+	hasher := md5.New()
+	if _, err := hasher.Write([]byte(key)); err != nil {
+		return nil, err
+	}
+
+	return hasher.Sum(nil), nil
 }
