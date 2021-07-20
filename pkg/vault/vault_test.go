10a7aabd21c300603db4ac4f3eec2224c2098b25
Georgy Serga <georgy.serga@gmail.com>
2021-07-20 10:10:58 -0400

    Remove unused private test

--- pkg/vault/vault_test.go
+++ pkg/vault/vault_test.go
@@ -6,31 +6,6 @@ import (
 	"testing"
 )
 
-func TestVault_loadData(t *testing.T) {
-	type fields struct {
-		encodingKey string
-		data        map[string]string
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			v := &Vault{
-				encodingKey: tt.fields.encodingKey,
-				data:        tt.fields.data,
-			}
-			if err := v.loadData(); (err != nil) != tt.wantErr {
-				t.Errorf("Vault.loadData() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
 func TestNew(t *testing.T) {
 	tests := []struct {
 		name    string
